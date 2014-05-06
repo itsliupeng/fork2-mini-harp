aa3095550671ce9e20aaf45cc5f375a1525f441f
liupeng <dp90219@gmail.com>
2014-05-06 14:56:38 +0800

    Implement A Current Time Middleware

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ var connect = require("connect");
 var app;
 
 module.exports = function(port) {
-  app = connect();
+  app = connect().use(function(req, res, next){
+    if(req.url == '/current-time') {
+      res.end(new Date().toISOString());
+    } else {
+      next();
+    }
+
+  });
   return app;
 }
 
