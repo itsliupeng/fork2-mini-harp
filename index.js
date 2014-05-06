5c8cc87fe7f6c74c580409de08ca806f6d888ab9
liupeng <dp90219@gmail.com>
2014-05-06 21:22:20 +0800

    Rendering Jade Templates

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
+var fs = require("fs");
 var connect = require("connect");
 var serveStatic = require("serve-static");
+var makeJade = require("./lib/processor/jade")
 var app;
 
 module.exports = function(root) {
-  app = connect().use(function(req, res, next){
-    if(req.url == '/current-time') {
-      res.end(new Date().toISOString());
-    } else {
-      next();
-    }
-  }).use(serveStatic(root));
+  app = connect()
+    .use(function(req, res, next){
+      if(req.url == '/current-time') {
+        res.end(new Date().toISOString());
+      } else {
+        next();
+      }
+    })
+    .use(serveStatic(root))
+    .use(makeJade(root));
 
   return app;
 }
